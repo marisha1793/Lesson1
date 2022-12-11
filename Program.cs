f4f1f70b012dfa03cf7986a9b8f5f4b2e268eafc
Mariya <marisha_1793@mail.ru>
2022-12-11 14:24:42 +0700

    первая задача

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("максимальное число равно ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("максимальное число равно ");
+    Console.WriteLine(b);
+}
